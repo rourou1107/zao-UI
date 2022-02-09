e99abc0a8d146771c348f3ce9bf266df152286dd
zhangrp <zhangrp@bairuitech.com>
2022-02-09 20:17:36 +0800

    增加Doc默认路由
    Change-Id: I05382fcef30ad6a8e46a2ae66867dd354d3c1f85

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { createWebHashHistory, createRouter } from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
+import Default from './components/Default.vue'
 import Switch from './components/Switch.vue'
 import Button from './components/Button.vue'
 import Dialog from './components/Dialog.vue'
@@ -12,6 +13,7 @@ export const router = createRouter({
         { path: '/', component: Home },
         { path: '/Doc', component: Doc,
             children: [
+                { path: '', component: Default },
                 { path: 'switch', component: Switch },
                 { path: 'button', component: Button },
                 { path: 'dialog', component: Dialog },
