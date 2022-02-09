c36c904acb936f6f7c17adb2e3ab2210778696b4
zhangrp <zhangrp@bairuitech.com>
2022-02-09 20:12:35 +0800

    Do整体样式修改
    Change-Id: I6a3f980d331730ec8eb12868779a3cf96d57dc5e

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,3 +1,61 @@
 <template>
-    <div>Switch</div>
+    <div>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+        <p>Switch 的文档</p>
+
+    </div>
 </template>
