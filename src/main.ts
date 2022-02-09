1b1896a321db0e9a2e4857dc632ed12db03b609d
zhangrp <zhangrp@bairuitech.com>
2022-02-09 20:07:53 +0800

    增加跳转路由
    Change-Id: Ic7500630f889ff5c15b041737a9153be77fffe40

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
+import {router} from './router';
+
 import './index.scss'
-import { createWebHashHistory, createRouter } from 'vue-router'
-const history = createWebHashHistory()
-const router = createRouter({
-    history,
-    routes: [
-        { path: '/', component: Home },
-        { path: '/Doc', component: Doc }
-    ]
-})
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
