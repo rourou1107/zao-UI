e6f254eb7c80d7b99ab3f525f65c7c1ce5d67021
zhangrp <zhangrp@bairuitech.com>
2022-02-09 16:33:03 +0800

    初始化aside列表
    Change-Id: I35dd6cd2747cdddfafecf7ba43b4ea35bd19381f

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
             <h1>Vue3 UI 框架</h1>
             <h2>一个便捷的 UI 框架</h2>
             <p class="actions">
-                <a>GitHub</a>
-                <a>开始</a>
+                <a href="https://github.com">GitHub</a>
+                <router-link to="/doc">开始</router-link>
             </p>
         </div>
     </div>
