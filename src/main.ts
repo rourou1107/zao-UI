b86f60c88b3331a61e24faecbab17731568801c0
zhangrp <zhangrp@bairuitech.com>
2022-02-09 15:48:53 +0800

    搭建主页以及文档页面的html
    Change-Id: Iee703d6e2c40dc9f257707e1876fb6994f14e002

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Text from './components/Text.vue'
-import Home from './components/Home.vue'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 import './index.css'
 import { createWebHashHistory, createRouter } from 'vue-router'
 const history = createWebHashHistory()
 const router = createRouter({
     history,
     routes: [
-        { path: '/', component: Text },
-        { path: '/home', component: Home }
+        { path: '/', component: Home },
+        { path: '/Doc', component: Doc }
     ]
 })
 const app = createApp(App)
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="topnav">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </div>
+        <div class="banner">
+            <h1>Vue3 UI 框架</h1>
+            <h2>一个便捷的 UI 框架</h2>
+            <p class="actions">
+                <a>GitHub</a>
+                <a>开始</a>
+            </p>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped></style>
