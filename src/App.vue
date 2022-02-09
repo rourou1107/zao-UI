fc5a974e7d8fb6b9c7836c2aa514cf17340beeee
zhangrp <zhangrp@bairuitech.com>
2022-02-09 15:18:16 +0800

    让ts识别vue文件
    Change-Id: If545be3af899d3a53479a3222f899c9089f0cca0

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <Text />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Text from './components/Text.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Text
   }
 }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import { createWebHashHistory, createRouter } from 'vue-router'
 
 createApp(App).mount('#app')
