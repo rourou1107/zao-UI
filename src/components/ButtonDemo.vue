be11b3daa024383cca9c2da982dcc7359287c440
zhangrp <zhangrp@bairuitech.com>
2022-02-10 16:00:34 +0800

    支持size属性
    Change-Id: Ibf3fb6cee0b231a07051fee08d163901c1627d71

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -4,7 +4,16 @@
     <Button theme="button">按钮</Button>
     <Button theme="link">链接按钮</Button>
     <Button theme="text">文字按钮</Button>
-
+    <p>示例二：size属性的使用</p>
+    <Button size="big">大</Button>
+    <Button>中</Button>
+    <Button size="small">小</Button><br/>
+    <Button theme="link" size="big">大</Button>
+    <Button theme="link">中</Button>
+    <Button theme="link" size="small">小</Button><br/>
+    <Button theme="text" size="big">大</Button>
+    <Button theme="text">中</Button>
+    <Button theme="text" size="small">小</Button>
 </template>
 <script lang="ts">
     import Button from '../lib/Button.vue'
@@ -15,6 +24,6 @@
 </script>
 <style lang="scss" scoped>
     p {
-        margin-bottom: 16px;
+        margin: 16px 0;
     }
 </style>
