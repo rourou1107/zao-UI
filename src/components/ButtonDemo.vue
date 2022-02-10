53c76d2eb04daebd3591c3232ecfcac83d41df6b
zhangrp <zhangrp@bairuitech.com>
2022-02-10 14:43:46 +0800

    增加theme并编写css
    Change-Id: I81ed3b92932cc354c64c1391a30c335aabbff744

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,15 +1,14 @@
 <template>
-    <Button @click="onClick" @mouseover="onClick" size="small">按钮</Button>
+    <Button>按钮</Button>
+    <Button theme="button">按钮</Button>
+    <Button theme="link">按钮</Button>
+    <Button theme="text">按钮</Button>
+
 </template>
 <script lang="ts">
     import Button from '../lib/Button.vue'
     export default {
         components: { Button },
-        setup() {
-            const onClick = () => {
-                console.log('hi')
-            }
-            return { onClick }
-        }
+        setup() {}
     }
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,15 +1,45 @@
 <template>
-    <div :size="size">
-        <button v-bind="rest"><slot /></button>
-    </div>
-
+    <button class="ui-button" :class="{[`theme-${theme}`]: theme}"><slot /></button>
 </template>
 <script lang="ts">
     export default {
-        inheritAttrs: false,
-        setup(props, context) {
-            const { size, ...rest } = context.attrs
-            return { size, rest }
+        props: {
+            theme: {
+                type: String,
+                default: 'button'
+            }
         }
     }
 </script>
+<style lang="scss" scoped>
+    $h: 32px;
+    $border-color: #d9d9d9;
+    $color: #333;
+    $blue: #40a9ff;
+    $radius: 4px;
+    .ui-button{
+        box-sizing: border-box;
+        height: $h;
+        padding: 0 12px;
+        cursor: pointer;
+        display: inline-block;
+        justify-content: center;
+        align-items: center;
+        white-space: nowrap;
+        background: #ffffff;
+        color: $color;
+        border: 1px solid $border-color;
+        border-radius: $radius;
+        box-shadow: 0 1px 0 fade-out(black, 0.95);
+        & + & {
+            margin-left: 8px;
+        }
+        &:hover, &:focus {
+            color: $blue;
+            border-color: $blue;
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
