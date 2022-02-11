d5b00ef41ff9c68bf7da3e382ccef1c745858e63
zhangrp <zhangrp@bairuitech.com>
2022-02-11 13:14:28 +0800

    增加visible属性来控制dialog开关
    Change-Id: I94320c9ca29576479af0e1048984f3f24a5bac7e

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,9 +1,19 @@
 <template>
-    <Dialog />
+    <Button @click="toggle">切换dialog</Button>
+    <Dialog v-model:visible="visible" />
 </template>
 <script lang="ts">
-    import Dialog from '../lib/Dialog.vue'
+    import Dialog from '../lib/Dialog.vue';
+    import Button from '../lib/Button.vue';
+    import { ref } from 'vue'
     export default {
-        components: { Dialog }
+        components: {Button, Dialog },
+        setup() {
+            const visible = ref(false)
+            const toggle = () => {
+                visible.value = !visible.value
+            }
+            return { toggle, visible }
+        }
     }
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,23 +1,37 @@
 <template>
-    <div class="gulu-dialog-overlay"></div>
-    <div class="gulu-dialog-wrapper">
-        <div class="gulu-dialog">
-            <header>标题 <span class="gulu-dialog-close"></span></header>
-            <main>
-                <p>第一行字</p>
-                <p>第二行字</p>
-            </main>
-            <footer>
-                <Button>OK</Button>
-                <Button>Cancel</Button>
-            </footer>
+    <template v-if="visible">
+        <div class="gulu-dialog-overlay"></div>
+        <div class="gulu-dialog-wrapper">
+            <div class="gulu-dialog">
+                <header>标题 <span class="gulu-dialog-close" @click="close"></span></header>
+                <main>
+                    <p>第一行字</p>
+                    <p>第二行字</p>
+                </main>
+                <footer>
+                    <Button>OK</Button>
+                    <Button>Cancel</Button>
+                </footer>
+            </div>
         </div>
-    </div>
+    </template>
 </template>
 <script lang="ts">
     import Button from '../lib/Button.vue'
     export default {
-        components: { Button }
+        components: { Button },
+        props: {
+            visible: {
+                type: Boolean,
+                default: true
+            }
+        },
+        setup(props, context) {
+            const close = () => {
+                context.emit('update:visible', false)
+            }
+            return { close }
+        }
     }
 </script>
 <style lang="scss">
