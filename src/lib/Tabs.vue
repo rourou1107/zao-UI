0d55c923bc1c06b13597ee27c544878433b9a556
zhangrp <zhangrp@bairuitech.com>
2022-02-13 14:19:47 +0800

    Tabs子组件必须为 Tba
    Change-Id: I924be03fe53157a951e1d99ba252648029a7f637

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,3 +1,17 @@
 <template>
-    <div>Tabs 组件</div>
+    <component :is="defaults[0]" />
+    <component :is="defaults[1]" />
 </template>
+<script lang="ts">
+    import Tab from './Tab.vue'
+    export default  {
+        setup(props, context) {
+            const defaults = context.slots.default()
+            const index = defaults.findIndex(item => item.type !== Tab)
+            if (index > -1) {
+                throw Error('Tabs 子组件 必须为 Tab')
+            }
+            return { defaults }
+        }
+    }
+</script>
