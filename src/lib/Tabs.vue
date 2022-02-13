9da2f1faa5cf40ecfd75cdc422a63c8e76184000
zhangrp <zhangrp@bairuitech.com>
2022-02-13 14:50:52 +0800

    添加css
    Change-Id: I2cdfb70394e008505365d57b5c280a6cda39e83f

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,12 @@
 <template>
-    <div v-for="(t, index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c, index) in defaults" :is="c" :key="index" />
+    <div class="gulu-tabs">
+        <div class="gulu-tabs-nav">
+            <div v-for="(t, index) in titles" :key="index" class="gulu-tabs-nav-item">{{t}}</div>
+        </div>
+        <div class="gulu-tabs-content">
+            <component v-for="(c, index) in defaults" :is="c" :key="index" class="gulu-tabs-content-item" />
+        </div>
+    </div>
 </template>
 <script lang="ts">
     import Tab from './Tab.vue'
@@ -16,3 +22,29 @@
         }
     }
 </script>
+<style lang="scss">
+    $blue: #40a9ff;
+    $color: #333;
+    $border-color: #d9d9d9;
+    .gulu-tabs {
+        &-nav {
+            display: flex;
+            color: $color;
+            border-bottom: 1px solid $border-color;
+            &-item {
+                padding: 8px 0;
+                margin: 0 16px;
+                cursor: pointer;
+                &:first-child {
+                    margin-left: 0;
+                }
+                &.selected {
+                    color: $blue;
+                }
+            }
+        }
+        &-content {
+            padding: 8px 0;
+        }
+    }
+</style>
