4037c6be32a803506bda92861eec919daca3d7ef
zhangrp <zhangrp@bairuitech.com>
2022-02-15 10:46:12 +0800

    创建三个新路由
    Change-Id: If597bc29760554b3d44380f71cdbe0fd3ef25d2f

--- src/router.ts
+++ src/router.ts
@@ -6,6 +6,9 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
+import Intro from './views/Intro.vue'
+import Install from './views/Install.vue'
+import GetStarted from './views/GetStarted.vue'
 const history = createWebHashHistory()
 export const router = createRouter({
     history,
@@ -14,6 +17,9 @@ export const router = createRouter({
         { path: '/doc', component: Doc,
             children: [
                 { path: '', component: Default },
+                { path: 'intro', component: Intro },
+                { path: 'install', component: Install },
+                { path: 'get-started', component: GetStarted },
                 { path: 'switch', component: SwitchDemo },
                 { path: 'button', component: ButtonDemo },
                 { path: 'dialog', component: DialogDemo },
